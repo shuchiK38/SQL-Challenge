bb7f46660794aedc97eca35333ec868f9ef8c8db
shuchi <shuchi.khandelwal@gmail.com>
2021-08-03 08:03:42 -0500

    newtable

--- EmployeeSQL/analysis.sql
+++ EmployeeSQL/analysis.sql
@@ -16,13 +16,18 @@ WHERE hire_date = 1986;
 
 SELECT dept_manager.dept_no, dept_manager.emp_no, departments.dept_name,employee.last_name, employee.first_name
 FROM  dept_manager
-LEFT JOIN departments.dept_name
-ON dept_manager.dept_no
-INNER JOIN employee
-ON dept_manager.emp_no = employee.emp_no 
+JOIN departments.dept_name
+ON dept_manager.dept_no=dept_manager.dept_no
+JOIN employee
+ON dept_manager.emp_no = employee.emp_no; 
 
 --4. List the department of each employee with the following information: employee number, last name, first name, and department name.
-
+SELECT employee.emp_no,employee.last_name,employee.first_name,departments.dept_no
+FROM departments
+JOIN employee
+ON departments.emp_no=employee.emp_no
+JOIN departments
+ON employee.dept_no=departments.dept_no;
 --5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
